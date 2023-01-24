9b416c0084b57d21eaf351292d411dde50c0916f
wzn <116744997+wzn6@users.noreply.github.com>
2023-01-24 14:41:54 +0000

    Update index.js

--- index.js
+++ index.js
@@ -24,3 +24,12 @@ connectiondb.start();
 client.login(process.env.token)
 
 //ANTICRASH
+process.on('unhandRejection', (reason, promise) => {
+    console.log(`❗ | [Erro]\n\n` + reason, promise);
+  });
+  process.on('uncaughtException', (error, origin) => {
+    console.log(`❗ | [Erro]\n\n` + error, origin);
+  });
+  process.on('uncaughtExceptionMonitor', (error, origin) => {
+    console.log(`❗ | [Erro]\n\n` + error, origin);
+  });
